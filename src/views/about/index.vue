903073e1a544fe41b0fa6088673cc2962185712a
aulor.xe <aulor.xe@outlook.com>
2022-04-10 18:55:11 +0800

    add naive ui & unplugin

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>about</div>
+    <n-grid>
+        <n-gi :span="12">
+            <n-tag>about</n-tag>
+        </n-gi>
+    </n-grid>
 </template>
 <script lang="ts" setup>
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+// import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        // AutoImport({ resolvers: [NaiveUiResolver()] }),
+        Components({ resolvers: [NaiveUiResolver()] })
+    ],
     resolve: {
         alias: { '@': path.resolve(__dirname, 'src') }
     }
